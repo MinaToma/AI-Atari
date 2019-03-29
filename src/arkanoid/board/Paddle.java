d19a7afb5751153ecafa523208f35e68dfaed672
ZiadAbdElrahman <iziad2000@outlook.com>
2019-03-29 17:19:53 +0200

    paddel class

--- src/arkanoid/board/Paddle.java
+++ src/arkanoid/board/Paddle.java
@@ -1,5 +1,66 @@
-package arkanoid.board;
 
-public class Paddle {
+
+package arkanoid.board;
+import atariCore.BaseObject;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class Paddle extends BaseObject{
+
+
+    public Paddle(int xPostion , int yPostion , Image image, int velX, int velY){
+        super(xPostion,yPostion,image, velX ,0);
+
+
+    }
+
+    public void tick(){
+        super.x += super.velX ;
+
+        if(super.x<=0)
+            x= 0 ;
+//        if(super.x >= " " - super.imageWidth )
+//            x = " " - super.imageWidth  ;
+    }
+
+    public void render(Graphics g){
+        g.drawImage(super.img, super.x ,super.y, null);
+
+    }
+
+
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) {
+            super.velX = -1;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            super.velX  = 1;
+        }
+    }
+
+
+    public void keyReleased(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) {
+            super.velX = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            super.velX = 0;
+        }
+    }
+
+
 
 }
+
+
+
