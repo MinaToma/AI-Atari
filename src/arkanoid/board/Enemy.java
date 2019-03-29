f6ecf017f81822ccc1371a399f47e606056d6cba
ZiadAbdElrahman <iziad2000@outlook.com>
2019-03-29 18:52:39 +0200

    Enemy class

--- src/arkanoid/board/Enemy.java
+++ src/arkanoid/board/Enemy.java
@@ -1,5 +1,37 @@
 package arkanoid.board;
 
-public class Enemy {
+import arkanoid.Common;
+import atariCore.BaseObject;
 
+import java.awt.*;
+
+public class Enemy extends BaseObject  implements Common {
+
+    private int power ;
+    public Enemy(int xPostion , int yPostion , Image image){
+        super(xPostion,yPostion, image);
+        power = 10 ;  // we can change it
+    }
+
+    public void render(Graphics g){
+        g.drawImage(super.img, super.x ,super.y, null);
+
+    }
+    public void tick() {
+        super.x += super.velX;
+        super.y += super.velY;
+
+        if (super.y <= 0 || super.y >= HEIGTH - super.imageHeight)
+            super.velY *= -1;
+        if (super.x <= 0 || super.x >= HEIGTH - super.imageHeight)
+            super.velY *= -1;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getPower() {
+        return power;
+    }
 }
