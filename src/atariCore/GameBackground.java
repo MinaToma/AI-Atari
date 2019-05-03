e90f92142cb34587155beeac2b665aece244135a
Mariam Mostafa <mariam.m.ali273@gmail.com>
2019-05-03 11:16:50 +0200

    Game Background Javadoc.

--- src/atariCore/GameBackground.java
+++ src/atariCore/GameBackground.java
@@ -2,36 +2,71 @@ package atariCore;
 
 import java.awt.*;
 
+/**
+ * Sets background images in gameplay.
+ */
 public class GameBackground extends BaseObject {
 
-    private static int cnt = 0;
+    /**
+     * counter to update GIF frames.
+     */
+    private static int count = 0;
+    /**
+     * limits frame updates to a specific time.
+     */
     private int timer = 0;
+    /**
+     * number of GIF frames.
+     */
     private int length;
+    /**
+     * background images array.
+     */
     private Image[] images;
 
+    /**
+     * Parameterised constructor used to set a single image background.
+     * @param x image x-coordinate.
+     * @param y image y-coordinate.
+     * @param img image to bet set as background.
+     */
     public GameBackground(float x, float y, Image img) {
         super(x, y, img);
-        cnt = 0;
+        count = 0;
         length = 0;
     }
 
+    /**
+     * Parameterised constructor used to set a multiple image background.
+     * @param x image x-coordinate.
+     * @param y image y-coordinate.
+     * @param images images  array (GIF frames).
+     * @param length number of frames.
+     */
     public GameBackground(float x, float y, Image[] images, int length) {
         super(x, y, images[0]);
         this.length = length;
         this.images = images;
     }
 
+    /**
+     * Updates game background every frame.
+     */
     @Override
     protected void tick() {
         if (length > 0) {
             timer++;
             if (timer % 10 == 0) {
-                this.setImage(images[cnt++]);
-                cnt %= 8;
+                this.setImage(images[count++]);
+                count %= length;
             }
         }
     }
 
+    /**
+     * Draws background image every frame.
+     * @param g Graphics object inside a runnable thread.
+     */
     @Override
     protected void render(Graphics g) {
         g.drawImage(image, 0, 0, null);
