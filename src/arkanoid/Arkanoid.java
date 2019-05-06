0fd5bcc71f2cd3b97cb8c2e5d9105ec82c0bd866
mehisen <mahmoud.mehisen@gmail.com>
2019-05-07 01:46:00 +0200

    fix bugs

--- src/arkanoid/Arkanoid.java
+++ src/arkanoid/Arkanoid.java
@@ -94,7 +94,7 @@ public class Arkanoid extends atariCore.Game {
         Handler.getInstance().addObject(playerList, player);
         Handler.getInstance().addObject(paddleList, p);
         setBackGround();
-        //setBricks(level);
+        setBricks(level);
 
         if (AIMode)
             p.setX(Helper.screenHeight * 0.1f);
